c0183c50b81a5d4da41c79b8c6bb7e24fe4570c2
KinyuaNgugi <kinyuag92@gmail.com>
2015-05-08 08:53:53 +0300

    first copy of the day

---
+++ models/Employee.java
@@ -0,0 +1,68 @@
+package com.springexample.models;
+
+import javax.persistence.*;
+
+/**
+ * Created by derric on 5/5/15.
+ */
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "first_name")
+    private String fname;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
